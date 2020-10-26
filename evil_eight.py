44151c892243796dc93a4f98330284b023b41600
fiolet <52757372+sarah-lishin@users.noreply.github.com>
2020-10-26 13:26:30 -0400

    Update evil_eight.py

--- evil_eight.py
+++ evil_eight.py
@@ -30,11 +30,11 @@ will = "will"
 who_list = ["A Ghouly, Gruesome Ghost", "Your Zombie Mother and Father", "The Devil Himself", "Frankenstein and his Monster", "Evil Elves to Steal Your Shoes", "A Slime Monster from the Black Lagoon", "Your Worst Nightmare"]
 what_list = ["You Will Surely Die", "Your Limbs Will Rot", "The Hounds Will Find You", "A Cauldron Will Fall on Your Head", "The Seer Requests a Smarter Question", "All You Love Will Dessicate"]
 when_list = ["The Seer Sees no Future Here", "After The Mountains Turn to Dust", "Once the Hounds Have Eaten Their Fill", "When the Great Pumpkin Rises", "Never, you Fool", "At the Stroke of Midnight"]
-where_list = ["In the Shrieking Hall of the Haunted Library", "Under the Rotted Apple Tree by the Cemetery", "Beneath the Coffin of Dracula", "It's All in Your Head, Mwahaha", "Within the Fold of the Mummy's Wrap"]
-why_list = ["Because You Should Know Your Death", "All Good Things Must Eventually Die", "You Should Have Not Opened the Coffin", "To Steal Your Soul", "The Evil Wrongs Need to be Corrected - With Your Death"]
-how_list = ["With Bat Saliva and Elbow Grease (Made From Elbows, of Course)", "With Three Turns of the Witches Broom", "Using the Blood of Twenty Virgins", "Your Question Is Irrelevant, Ask Again", "With Screams, of Course"]
-will_list = ["It will Never Happen Unless it is a Full Moon.", "Only if you Defeat the Vampires", "Should the Witches Agree, then Yes", "On the Third Night After the Ghouls Rise"]
-other_list = ["Question Unclear, Try Again Meatbag", "You Never Know With These Things, Try Boiling It", "Deadly Nightshade Usually Works", "After You Choke, Of Course", "Try Brains, They Might Satisfy Your Cravings"]
+where_list = ["In the Shrieking Hall of the Haunted Library", "Under the Rotted Apple Tree by the Cemetery", "Beneath the Coffin of Dracula", "It's All in Your Head, Mwahaha", "Within the Fold of the Mummy's Wrap", "Hidden in the Cyrpts"]
+why_list = ["Because You Should Know Your Death", "All Good Things Must Eventually Die", "You Should Have Not Opened the Coffin", "To Steal Your Soul", "The Evil Wrongs Need to be Corrected - With Your Death", "To Plunder the Earth and Spill Blood"]
+how_list = ["With Bat Saliva and Elbow Grease (Made From Elbows, of Course)", "With Three Turns of the Witches Broom", "Using the Blood of Twenty Virgins", "Your Question Is Irrelevant, Ask Again", "With Screams, of Course", "With a Flabled Dark Curse"]
+will_list = ["It Will Never Happen Unless it is a Full Moon.", "Only if you Defeat the Vampires", "Should the Witches Agree, then Yes", "On the Third Night After the Ghouls Rise", "Not With a Stuid Question Like That, Ask Again"]
+other_list = ["Question Unclear, Try Again Meatbag", "You Never Know With These Things, Try Boiling It", "Deadly Nightshade Usually Works", "After You Choke, Of Course", "Try Brains, They Might Satisfy Your Cravings", "Zombies Might Eat You, But You Should Try Anyways"]
 
 
 who_random = random.choice(who_list)
